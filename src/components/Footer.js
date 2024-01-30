0823a47c4ae1f520ecdf8293f432757e952848e8
leobzt <72006737+leobzt@users.noreply.github.com>
2024-01-30 11:58:01 +0100

    init

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+// src/components/Footer.js
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>© 2024 Mon Super Site. Tous droits réservés.</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/pages/LandingPage.js
@@ -0,0 +1,13 @@
+// src/pages/LandingPage.js
+import React from 'react';
+
+function LandingPage() {
+  return (
+    <div className="landing-page">
+      <h2>Bienvenue sur notre landing page !</h2>
+      <p>C'est une page simple créée avec React.</p>
+    </div>
+  );
+}
+
+export default LandingPage;
